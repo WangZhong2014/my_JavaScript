937caa583a13c8f7d1b1f09506264e82a3d3c4ba
WangZhong2014 <992091667@qq.com>
2018-03-31 10:01:46 +0800

    note for 4

--- 犀牛书学习笔记/2.js
+++ 犀牛书学习笔记/2.js
@@ -1,11 +1,10 @@
-var obj = {
-    count: 1
-  }
-  
-  var myArray = ['red', 'white', 'black'];
-  console.log(myArray.length);
-  
-  myArray.obj = obj;
-  console.log(myArray);
-  console.log(myArray.length);
-  
+var o = {x:1,y:{z:3},ab_10:"道道"};
+var a = [o,4,[5,6]];
+
+console.log(o.x);
+console.log(o.y.z);
+console.log(o['x']);
+console.log(a[1]);
+console.log(a[1][1]);
+console.log(a[2][1]);
+console.log(o.ab_10);
