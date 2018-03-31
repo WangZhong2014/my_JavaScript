5511d69a60fefa19e3235fdcde39986de79c49f2
WangZhong2014 <992091667@qq.com>
2018-03-31 10:01:13 +0800

    第四章

---
+++ 犀牛书学习笔记/2.js
@@ -0,0 +1,11 @@
+var obj = {
+    count: 1
+  }
+  
+  var myArray = ['red', 'white', 'black'];
+  console.log(myArray.length);
+  
+  myArray.obj = obj;
+  console.log(myArray);
+  console.log(myArray.length);
+  
